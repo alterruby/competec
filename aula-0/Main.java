00aa5b48209e57a2cfa39297aacfd10669279ba6
alterruby <isabella.lobo2009@gmail.com>
2025-04-26 15:11:12 -0300

    Update Main.java

--- aula-0/Main.java
+++ aula-0/Main.java
@@ -2,17 +2,6 @@ public class Main
 {
 	public static void main(String[] args) {
         // Escreva seu código aqui ⬇️
-            char PrimeiraLetraDoMeuNome = 'I';
-	     
-	    int MinhaIdade = 15;
-	    
-	     boolean TomeicaféDaManhã = true;
-	    
-		System.out.println(PrimeiraLetraDoMeuNome);
-		
-		System.out.println(MinhaIdade);
-		
-		System.out.println(TomeicaféDaManhã);
 		
 	}
 }
